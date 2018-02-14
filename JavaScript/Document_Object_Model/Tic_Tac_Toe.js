e0edcf7828f0df0b4a3c6b41449cca1cbaf8bc2c
MikiEEE <mikemp1997@gmail.com>
2018-02-13 19:11:37 -0600

    Document Object Model Exercise

---
+++ JavaScript/Document_Object_Model/Tic_Tac_Toe.js
@@ -0,0 +1,12 @@
+
+
+
+
+var zero0 = document.querySelector('#zero0');
+var zero1 = document.querySelector('#zero0');
+var zero2 = document.querySelector('#zero0');
+
+
+zero0.addEventListener('click', function() {
+  zero0.innerHTML= 'X';
+});
