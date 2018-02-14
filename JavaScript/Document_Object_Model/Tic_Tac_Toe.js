2ec2ec8886d802472309edf1080b6fb168625198
MikiEEE <mikemp1997@gmail.com>
2018-02-14 09:26:15 -0600

    Added Reset Functionality

--- JavaScript/Document_Object_Model/Tic_Tac_Toe.js
+++ JavaScript/Document_Object_Model/Tic_Tac_Toe.js
@@ -3,21 +3,29 @@
 
 
 var divisions = document.querySelectorAll("td");
+var button = document.querySelector("#form");
 
+for(var i = 0; i < divisions.length; i++) {
+  divisions[i].addEventListener('click',changeSymbol);
+}
 
+button.addEventListener('click', clear);
 
+function clear() {
+  for(var i = 0; i < divisions.length; i++) {
+    divisions[i].textContent = '';
+  }
+}
 
-  function changeSymbol(){
-      if(this.textContent === ''){
-        this.textContent = 'X';
-        // console.log(marker)
-      }else if (this.textContent === 'X') {
-        this.textContent = 'O';
-      }else {
-        this.textContent = '';
+  function changeSymbol() {
+    if(this.textContent === '') {
+      this.textContent = 'X';
+      // console.log(marker)
       }
-    }
-
-    for(var i = 0; i < divisions.length; i++) {
-      divisions[i].addEventListener('click',changeSymbol);
-    }
+    else if (this.textContent === 'X') {
+      this.textContent = 'O';
+      }
+    else {
+      this.textContent = '';
+      }
+}
