8b912f461efc1e1e4363ccbe424ba6fa664f6de6
MikiEEE <mikemp1997@gmail.com>
2018-02-13 23:02:52 -0600

    Adding to Tic Tac Toe

--- JavaScript/Document_Object_Model/Tic_Tac_Toe.js
+++ JavaScript/Document_Object_Model/Tic_Tac_Toe.js
@@ -2,11 +2,22 @@
 
 
 
-var zero0 = document.querySelector('#zero0');
-var zero1 = document.querySelector('#zero1');
-var zero2 = document.querySelector('#zero2');
+var divisions = document.querySelectorAll("td");
 
 
-zero0.addEventListener('click', function() {
-  zero0.innerHTML= 'X';
-});
+
+
+  function changeSymbol(){
+      if(this.textContent === ''){
+        this.textContent = 'X';
+        // console.log(marker)
+      }else if (this.textContent === 'X') {
+        this.textContent = 'O';
+      }else {
+        this.textContent = '';
+      }
+    }
+
+    for(var i = 0; i < divisions.length; i++) {
+      divisions[i].addEventListener('click',changeSymbol);
+    }
