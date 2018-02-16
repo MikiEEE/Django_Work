1e9c2395de91b4408ec30919852d946f8fa87ba3
MikiEEE <mikemp1997@gmail.com>
2018-02-15 19:16:00 -0600

    three in a row detection added

--- JavaScript/Document_Object_Model/Tic_Tac_Toe.js
+++ JavaScript/Document_Object_Model/Tic_Tac_Toe.js
@@ -29,33 +29,36 @@ function changeSymbol() {
   else {
     this.textContent = '';
   }
-  for(var i = 100; i < 0; i--)
-    continue;
   testForThree();
 }
 
 function testForThree() {
   var count = 0;
-  for(var i = 0; i < divisions.length; i++) {
+  var save;
+  for(var i = 0; i < 3; i++) {
     if(divisions[i].textContent != '') {
-      switch(i) {
-        case 0:
-        case 2:
-        case 6:
-        case 8:
-          count+=3;
-          break;
-        case 1:
-        case 3:
-        case 4:
-        case 5:
-        case 7:
-          count+=2;
+      if(divisions[i].textContent === divisions[i + 3].textContent && divisions[i + 6].textContent == divisions[i].textContent) {
+        console.log("Winner");
+        break;
       }
     }
-    if(count === 6|| count === 8) {
+  }
+  for(var i = 0; i < 9; i+=3) {
+    if(divisions[i].textContent != '') {
+      if(divisions[i].textContent === divisions[i + 1].textContent && divisions[i].textContent === divisions[i + 2].textContent) {
+        console.log("Winner");
+        break;
+      }
+    }
+  }
+  if(divisions[2].textContent != ''){
+    if(divisions[2].textContent === divisions[4].textContent && divisions[6].textContent === divisions[4].textContent) {
       console.log("winner");
-      break;
+    }
+  }
+  if (divisions[0].textContent != '') {
+    if(divisions[0].textContent === divisions[4].textContent && divisions[8].textContent === divisions[4].textContent) {
+      console.log("Winner");
     }
   }
 }
