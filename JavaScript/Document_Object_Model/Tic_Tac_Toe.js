b5e5b6c0162dc42b472e3d6d331262583f4cb4df
MikiEEE <mikemp1997@gmail.com>
2018-02-17 13:36:19 -0600

    Cleaned up some statments in the Tic_Tac_Toe.js

--- JavaScript/Document_Object_Model/Tic_Tac_Toe.js
+++ JavaScript/Document_Object_Model/Tic_Tac_Toe.js
@@ -4,7 +4,9 @@
 
 var divisions = document.querySelectorAll("td");
 var button = document.querySelector("#form");
-var temp;
+var p = document.querySelector("p");
+var count = 0;
+var newGame = true;
 
 for(var i = 0; i < divisions.length; i++) {
   divisions[i].addEventListener('click',changeSymbol);
@@ -12,23 +14,29 @@ for(var i = 0; i < divisions.length; i++) {
 
 button.addEventListener('click', clear);
 
+
 function clear() {
   for(var i = 0; i < divisions.length; i++) {
     divisions[i].textContent = '';
   }
+  p.textContent = '';
+  count = 0;
   return;
 }
 
 function changeSymbol() {
-  if(this.textContent === '') {
+  if(count % 2 == 0) {
     this.textContent = 'X';
-    }
-  else if (this.textContent === 'X') {
-    this.textContent = 'O';
-    }
-  else {
-    this.textContent = '';
   }
+  else {
+    this.textContent = 'O';
+  }
+  ++count;
+  if(count > 8 ) {
+    console.log("tie");
+    clear();
+  }
+  console.log(count);
   testForThree();
 }
 
@@ -37,28 +45,39 @@ function testForThree() {
   var save;
   for(var i = 0; i < 3; i++) {
     if(divisions[i].textContent != '') {
-      if(divisions[i].textContent === divisions[i + 3].textContent && divisions[i + 6].textContent == divisions[i].textContent) {
+      if(divisions[i].textContent === divisions[i + 3].textContent) {
+        if(divisions[i + 6].textContent == divisions[i].textContent) {
         console.log("Winner");
         break;
+        }
       }
     }
   }
   for(var i = 0; i < 9; i+=3) {
     if(divisions[i].textContent != '') {
-      if(divisions[i].textContent === divisions[i + 1].textContent && divisions[i].textContent === divisions[i + 2].textContent) {
-        console.log("Winner");
-        break;
+      if(divisions[i].textContent === divisions[i + 1].textContent) {
+        if (divisions[i].textContent === divisions[i + 2].textContent) {
+          console.log("Winner");
+          p.textContent = "WINNER";
+          break;
+        }
       }
     }
   }
   if(divisions[2].textContent != ''){
-    if(divisions[2].textContent === divisions[4].textContent && divisions[6].textContent === divisions[4].textContent) {
-      console.log("winner");
+    if(divisions[2].textContent === divisions[4].textContent) {
+      if(divisions[6].textContent === divisions[4].textContent) {
+        console.log("Winner");
+        p.textContent = "WINNER";
+      }
     }
   }
   if (divisions[0].textContent != '') {
-    if(divisions[0].textContent === divisions[4].textContent && divisions[8].textContent === divisions[4].textContent) {
-      console.log("Winner");
+    if(divisions[0].textContent === divisions[4].textContent) {
+      if(divisions[8].textContent === divisions[4].textContent) {
+        console.log("Winner");
+        p.textContent = "WINNER";
+      }
     }
   }
 }
