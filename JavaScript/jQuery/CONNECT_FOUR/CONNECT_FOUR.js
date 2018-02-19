d5c5398ebbb168db394bba828a37596371a38cdc
MikiEEE <mikemp1997@gmail.com>
2018-02-18 20:04:47 -0600

    Added Horizontal victory functionality

--- JavaScript/jQuery/CONNECT_FOUR/CONNECT_FOUR.js
+++ JavaScript/jQuery/CONNECT_FOUR/CONNECT_FOUR.js
@@ -1,6 +1,8 @@
 
 var turn = 0;
 var loopControl = new Array(7);
+
+
 for(var i = 0; i < $('button').length; i++) {
   $('button').eq(i).click(findGray);
 }
@@ -10,7 +12,6 @@ for(var d = 0; d < loopControl.length; d++) {
 }
 
 
-
 function findGray() {
   for(i = $(this).index('button') % 7; i < loopControl[$(this).index('button') % 7]; i+=7) {
   }
@@ -18,9 +19,38 @@ function findGray() {
    $('button').eq(i).addClass(toggle);
    loopControl[ i % 7] = i - 7;
   }
-  else if($('button').eq(i + 7).css('background-color') !== 'gray') {
+  else if($('button').eq(i + 7).css('background-color') !== 'rgb(128, 128, 128)') {
     $('button').eq(i).addClass(toggle);
     loopControl[i % 7] = i - 7;
+  }
+  testForWin();
+}
+
+function testForWin() {
+  for(var rows = 0; rows < 6; rows++) {
+    for(var column = 0; column < 8; column++) {
+      console.log($('button').eq((rows * 6) + column).css('background-color'));
+      if($('button').eq((rows * 6) + column).css('background-color') != 'rgb(128, 128, 128)') {
+        if($('button').eq((rows * 6) + column).css('background-color') == $('button').eq((rows * 6) + column + 1).css('background-color')) {
+          if($('button').eq((rows * 6) + column + 1).css('background-color') == $('button').eq((rows * 6) + column + 2).css('background-color')) {
+            if($('button').eq((rows * 6) + column + 2).css('background-color') == $('button').eq((rows * 6) + column + 3).css('background-color')) {
+              console.log("Winner is blue");
+              break;
+            }
+          }
+        }
+      }
+      if($('button').eq((rows * 6) + column).css('background-color') != 'rgb(128, 128, 128)') {
+        if($('button').eq((rows * 6) + column).css('background-color') == $('button').eq((rows * 6) + column + 1).css('background-color')) {
+          if($('button').eq((rows * 6) + column + 1).css('background-color') == $('button').eq((rows * 6) + column + 2).css('background-color')) {
+            if($('button').eq((rows * 6) + column + 2).css('background-color') == $('button').eq((rows * 6) + column + 3).css('background-color')) {
+              console.log("Winner is blue");
+              break;
+            }
+          }
+        }
+      }
+    }
   }
 }
 
