92308276739ae3621a9ca1ab0300675ef741b360
MikiEEE <mikemp1997@gmail.com>
2018-03-24 17:39:15 -0500

    Finished

--- Backend/python_part_1/Exercise_2.py
+++ Backend/python_part_1/Exercise_2.py
@@ -19,7 +19,7 @@
 def arrayCheck(nums):
     count = 0
     largest_in_sequence = 3
-    for x in nums[2::largest_in_sequence]:
+    for x in nums[2::]:
         if x == largest_in_sequence:
             for i in range(1,largest_in_sequence):
                 if nums[x - i] == x - i:
@@ -48,9 +48,9 @@ def stringBits(str):
     newstr = ''
     for x in str[::2]:
         newstr += x
-    print(newstr)
+    return(newstr)
 
-stringBits('Hi')
+print(stringBits('Hi'))
 #####################
 ## -- PROBLEM 3 -- ##
 #####################
@@ -75,76 +75,99 @@ def end_other(a, b):
     increment = 0
     counter = 0
     if(len(b) > len(a)):
-        print(False)
         return False
     for x in range(startingPoint,len(a)):
         if a[x] == b[counter]:
             increment += 1
             counter += 1
         if increment < counter:
-            print(False)
             return False
         elif increment == len(b) - 1:
-            print(True)
             return True
-    print(False)
     return False
 
-end_other('adbc', 'abc')
+print(end_other('adbc', 'abc'))
 
 
-# #####################
-# ## -- PROBLEM 4 -- ##
-# #####################
+#####################
+## -- PROBLEM 4 -- ##
+#####################
+
+# Given a string, return a string where for every char in the original,
+# there are two chars.
+
+# doubleChar('The') → 'TThhee'
+# doubleChar('AAbb') → 'AAAAbbbb'
+# doubleChar('Hi-There') → 'HHii--TThheerree'
+
+def doubleChar(str):
+    newstr = ''
+    for x in str:
+        for y in range(0,2):
+            newstr += x
+    return newstr
+
+print(doubleChar('Hi-There'))
+
+#####################
+## -- PROBLEM 5 -- ##
+#####################
+
+# Read this problem statement carefully!
+
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
 #
-# # Given a string, return a string where for every char in the original,
-# # there are two chars.
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
 #
-# # doubleChar('The') → 'TThhee'
-# # doubleChar('AAbb') → 'AAAAbbbb'
-# # doubleChar('Hi-There') → 'HHii--TThheerree'
+# Examples:
 #
-# def doubleChar(str):
-#   # CODE GOES HERE
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
+
+def no_teen_sum(a, b, c):
+    list = [a, b , c]
+    sumOfTeens = 0
+    for x in list:
+        sumOfTeens += fix_teen(x)
+    return sumOfTeens
+
+def fix_teen(n):
+    if(n == 15 or n == 16):
+        return n
+    elif(n > 12 and n < 19):
+        return 0
+    elif(type(n) == type(10)):
+        return n
+    else:
+        print('Error')
+        return 'Error'
+
+print(no_teen_sum(2,1,14))
+
+
+#####################
+## -- PROBLEM 6 -- ##
+#####################
+
+# Return the number of even integers in the given array.
 #
+# Examples:
 #
-# #####################
-# ## -- PROBLEM 5 -- ##
-# #####################
-#
-# # Read this problem statement carefully!
-#
-# # Given 3 int values, a b c, return their sum. However, if any of the values is a
-# # teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
-# # and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
-# # takes in an int value and returns that value fixed for the teen rule.
-# #
-# # In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
-# # Define the helper below and at the same indent level as the main no_teen_sum().
-# # Again, you will have two functions for this problem!
-# #
-# # Examples:
-# #
-# # no_teen_sum(1, 2, 3) → 6
-# # no_teen_sum(2, 13, 1) → 3
-# # no_teen_sum(2, 1, 14) → 3
-#
-# def no_teen_sum(a, b, c):
-#   # CODE GOES HERE
-# def fix_teen(n):
-#   # CODE GOES HERE
-#
-# #####################
-# ## -- PROBLEM 6 -- ##
-# #####################
-#
-# # Return the number of even integers in the given array.
-# #
-# # Examples:
-# #
-# # count_evens([2, 1, 2, 3, 4]) → 3
-# # count_evens([2, 2, 0]) → 3
-# # count_evens([1, 3, 5]) → 0
-#
-# def count_evens(nums):
-#   # CODE GOES HERE
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
+def count_evens(nums):
+    count = 0
+    for x in nums:
+        if(x%2 == 0):
+            count +=1
+    return count
+
+print(count_evens([2,1,2,3,4]))
